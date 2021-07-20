28eb33a2b8c713956d51109075d8a259f7d7b92f
abhiram11 <abhiramsatpute@gmail.com>
2021-07-20 18:03:51 +0530

    done with all the bugs

--- components/carItem/styles.js
+++ components/carItem/styles.js
@@ -4,7 +4,7 @@ const styles = StyleSheet.create({
   carContainer: { width: "100%", height: Dimensions.get("window").height },
   titles: { marginTop: "30%", width: "100%", alignItems: "center" },
   title: { fontSize: 40, fontWeight: "500" },
-  subtitle: { fontSize: 16, color: "#5c5e62" },
+  subtitle: { fontSize: 16, color: "#5c5e62" }, //'#5c5e62'
   subtitleCTA: { textDecorationLine: "underline" },
   image: {
     width: "100%",
