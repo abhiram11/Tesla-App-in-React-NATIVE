bfd40c61ba726cdf813e7b65a5525ceafb8cbb17
abhiram11 <abhiramsatpute@gmail.com>
2021-07-20 11:57:33 +0530

    added buttons, styling them to bottom of view

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -2,17 +2,23 @@ import React from "react";
 import { View, Text, Pressable } from "react-native";
 import styles from "./styles";
 
-const StyledButton = () => {
+const StyledButton = (props) => {
+  const type = props.type;
+  const content = props.content;
+  const onPress = props.onPress;
+  //   console.warn(type);
+
+  const backgroundColor = type === "primary" ? "#171A20CC" : "#FFFFFFA6";
+  const textColor = type === "primary" ? "#FFFFFF" : "#171A20";
+
   return (
     <View style={styles.container}>
       {/* <Text>StyledButton</Text> */}
       <Pressable
-        style={styles.button}
-        onPress={() => {
-          console.warn("You tapped ?");
-        }}
+        style={[styles.button, { backgroundColor: backgroundColor }]}
+        onPress={() => onPress()}
       >
-        <Text style={styles.text}> Custom Order!</Text>
+        <Text style={[styles.text, { color: textColor }]}>{content}</Text>
       </Pressable>
     </View>
   );
--- components/StyledButton/styles.js
+++ components/StyledButton/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: { width: "100%", padding: 10 },
   button: {
-    backgroundColor: "gray",
+    // backgroundColor: "gray",
     height: 40,
     justifyContent: "center",
     alignItems: "center",
